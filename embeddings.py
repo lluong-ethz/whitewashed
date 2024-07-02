35d286ef4a56ce603e95a38d12bf2451243094a1
minlfk <56965216+minlfk@users.noreply.github.com>
2024-07-02 17:19:58 +0200

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -12,66 +12,66 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 import re
 # import tf.keras.models
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Embedding, Lambda
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import skipgrams
-import tensorflow.keras.backend as K
-from tensorflow.keras.utils import to_categorical
-import numpy as np
-import matplotlib.pyplot as plt
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense,Embedding, Lambda
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import skipgrams
+# import tensorflow.keras.backend as K
+# from tensorflow.keras.utils import to_categorical
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
-def cbow(full = False):
-    tweets = []
-    dummy = []
+# def cbow(full = False):
+#     tweets = []
+#     dummy = []
 
-    # shouldn't we load the tweets outside the function?
-    if(not full):
-        load_tweets(SMALL_TRAIN_POS, 0, tweets, dummy)
-        load_tweets(SMALL_TRAIN_NEG, 1, tweets, dummy)
-    else:
-        load_tweets(TRAIN_POS, 0, tweets, dummy)
-        load_tweets(TRAIN_NEG, 0, tweets, dummy)
+#     # shouldn't we load the tweets outside the function?
+#     if(not full):
+#         load_tweets(SMALL_TRAIN_POS, 0, tweets, dummy)
+#         load_tweets(SMALL_TRAIN_NEG, 1, tweets, dummy)
+#     else:
+#         load_tweets(TRAIN_POS, 0, tweets, dummy)
+#         load_tweets(TRAIN_NEG, 0, tweets, dummy)
 
-    # Convert to NumPy array to facilitate indexing
-    tweets = np.array(tweets)
-    labels = np.array(labels)
+#     # Convert to NumPy array to facilitate indexing
+#     tweets = np.array(tweets)
+#     labels = np.array(labels)
 
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(tweets)
+#     tokenizer = Tokenizer()
+#     tokenizer.fit_on_texts(tweets)
 
-    vocab_size = len(tokenizer.word_index) + 1
-    sequences = tokenizer.texts_to_sequences(tweets)
+#     vocab_size = len(tokenizer.word_index) + 1
+#     sequences = tokenizer.texts_to_sequences(tweets)
 
-    window_size = 4
+#     window_size = 4
 
-    skip_grams = [skipgrams(seq, vocabulary_size=vocab_size, window_size=window_size, negative_samples = 0) for seq in sequences]
-    pairs, _ = skip_grams[0][0], skip_grams[0][1]
+#     skip_grams = [skipgrams(seq, vocabulary_size=vocab_size, window_size=window_size, negative_samples = 0) for seq in sequences]
+#     pairs, _ = skip_grams[0][0], skip_grams[0][1]
 
-    word_target, word_context = zip(*pairs)
-    word_target = np.array(word_target)
-    word_context = np.array(word_context)
+#     word_target, word_context = zip(*pairs)
+#     word_target = np.array(word_target)
+#     word_context = np.array(word_context)
 
-    embedding_size = 100
+#     embedding_size = 100
 
-    model = Sequential()
-    model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, input_length=2*window_size))
-    model.add(Lambda(lambda x: K.mean(x, axis=1), output_shape=(embedding_size,)))
-    model.add(Dense(vocab_size, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
+#     model = Sequential()
+#     model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, input_length=2*window_size))
+#     model.add(Lambda(lambda x: K.mean(x, axis=1), output_shape=(embedding_size,)))
+#     model.add(Dense(vocab_size, activation='softmax'))
+#     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    labels = to_categorical(labels, vocab_size)
+#     labels = to_categorical(labels, vocab_size)
 
-    model.fit([word_context], word_target, epochs=100, verbose=2)
+#     model.fit([word_context], word_target, epochs=100, verbose=2)
 
-    # Extract the word embeddings
-    word_embeddings = model.layers[0].get_weights()[0]
+#     # Extract the word embeddings
+#     word_embeddings = model.layers[0].get_weights()[0]
 
-    # Function to get embedding for a word
-    def get_embedding(word):
-        word_id = tokenizer.word_id[word]
-        return word_embeddings[word_id]
+#     # Function to get embedding for a word
+#     def get_embedding(word):
+#         word_id = tokenizer.word_id[word]
+#         return word_embeddings[word_id]
 
 # Load GloVe embeddings
 def load_glove_embeddings(glove_file):
@@ -130,21 +130,23 @@ load_tweets(SMALL_TRAIN_NEG, 1, tweets, labels)
 
 tweet_emb = {}
 
-tweet_emb['vector'] = tweets.apply(lambda x: tweet_to_glove_vector(x, embeddings_index))
-tweet_emb['label'] = labels
+#contains all the embeddings
+tweet_emb['vector'] = [tweet_to_glove_vector(tweet, embeddings_index) for tweet in tweets]
+# print(tweet_emb['vector'][:10])
+# tweet_emb['label'] = labels
 
-# Prepare features and labels
-X = np.array(tweet_emb['vector'].tolist())
-y = tweet_emb['label']  # Assuming 'label' column contains 0 for negative and 1 for positive
+# # Prepare features and labels
+# X = tweet_emb['vector']
+# y = tweet_emb['label']  # Assuming 'label' column contains 0 for negative and 1 for positive
 
-# Train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Train/test split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train a classifier
-clf = LogisticRegression()
-clf.fit(X_train, y_train)
+# # Train a classifier
+# clf = LogisticRegression()
+# clf.fit(X_train, y_train)
 
-# Predict and evaluate
-y_pred = clf.predict(X_test)
-print('Accuracy:', accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+# # Predict and evaluate
+# y_pred = clf.predict(X_test)
+# print('Accuracy:', accuracy_score(y_test, y_pred))
+# print(classification_report(y_test, y_pred))

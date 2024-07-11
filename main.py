f0ff43cf368653cf7b5c0482f2ffe1c8d3a24b76
Selim Jerad <selim.jerad@epfl.ch>
2024-07-11 17:37:05 +0200

    neural network

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from models.bow import *
 from utils import *
 from embeddings.glove import *
+from models.nn import *
+from torch.utils.data import DataLoader, TensorDataset
+import torch
+import pickle
 
 def main():
     full = False
@@ -19,17 +23,25 @@ def main():
     tweets = np.array(tweets)
     labels = np.array(labels)
 
-    # bow()
-    #glove_embeddings_path = 'data/glove_wiki/glove.6B.50d.txt'
-    #embeddings_index = load_glove_embeddings(glove_embeddings_path)
+    X_train, X_val, Y_train, Y_val = split_train_test(tweets, labels, 1)
 
-    # Load your data
-    #tweets = []
-    #labels = []
-    #load_tweets(SMALL_TRAIN_POS, 0, tweets, labels)
-    #load_tweets(SMALL_TRAIN_NEG, 1, tweets, labels)
-    #print(tweet_to_glove_vector(tweets[0], embeddings_index))
+    X_train = all_tweets_to_glove(X_train, GLOVE_WIKI_50D)
+    # Save the list to a file
+    with open('glove_train.pkl', 'wb') as f:
+        pickle.dump(X_train, f)
+    X_val = all_tweets_to_glove(X_val, GLOVE_WIKI_50D)
+    with open('glove_val.pkl', 'wb') as f:
+        pickle.dump(X_val, f)
 
+    train_dataset = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(Y_train))
+    test_dataset = TensorDataset(torch.from_numpy(X_val), torch.from_numpy(Y_val))
+
+    batch_size = 32
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    model = train_simple_nn(train_loader)
+    test_simple_nn(test_loader, model)
 
 if __name__ == '__main__':
     main()
---
+++ models/nn.py
@@ -0,0 +1,68 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from utils import *
+
+class simple_nn(nn.Module):
+    def __init__(self, input_dim = 50):
+        super(simple_nn, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 128)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = torch.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = self.fc3(x)
+        x = self.sigmoid(self.fc4(x))
+        return x
+
+def train_simple_nn(train_loader):
+    model = simple_nn()
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    epochs = 1
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs).squeeze()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader)}")
+    
+    torch.save(model, 'nn.pt')
+    return model
+
+def test_simple_nn(test_loader, model):
+    # Testing
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs).squeeze()
+            predicted = (outputs > 0.5).float()
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Test Accuracy: {correct / total:.4f}')
+    
+    
+
+
+
+

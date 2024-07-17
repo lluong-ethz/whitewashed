ffc162f33629ad22aaa55cc324df38fe1c4cbfa9
mmultani-ethz <124575566+mmultani-ethz@users.noreply.github.com>
2024-07-17 09:09:47 +0200

    improved tfidf with naive bayes

--- models/tfidf_naive_bayes.py
+++ models/tfidf_naive_bayes.py
@@ -1,31 +1,77 @@
 #!/usr/bin/env python3
-import pickle
+from collections import Counter
 import numpy as np
-from utils import *
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from utils import load_tweets, split_train_test, get_top_pos_neg, get_basic_metrics
 
 
-def tfidf_naive_bayes(tweets, labels):
-
-    X_train, X_val, Y_train, Y_val = split_train_test(tweets, labels, 1)
+def build_vocab(tweets):
+    vocabulary = {}
+    tokens = []
+    for tweet in tweets:
+        for word in tweet.split():
+            tokens.append(word)
 
     # We only keep the 5000 most frequent words, both to reduce the computational cost and reduce overfitting
-    vectorizer = TfidfVectorizer(max_features=5000)
+    counter = Counter(tokens)
+    most_common_words = counter.most_common(5000)
+    most_common_words_len = len(most_common_words)
 
-    # Important: we call fit_transform on the training set, and only transform on the validation set
-    X_train = vectorizer.fit_transform(X_train)
-    X_val = vectorizer.transform(X_val)
+    for index in range(most_common_words_len):
+        word = most_common_words[index][0]
+        vocabulary[word] = index
+    return vocabulary
+
+
+# train machine learning model to predict the label
+def main():
+    tweets = []
+    labels = []
+
+    # Load tweets
+    load_tweets('../data/train_neg.txt', 0, tweets, labels)
+    load_tweets('../data/train_pos.txt', 1, tweets, labels)
+    # load_tweets('../data/train_neg_full.txt', 0, tweets, labels)
+    # load_tweets('../data/train_pos_full.txt', 1, tweets, labels)
+
+    # Convert to NumPy array to facilitate indexing
+    tweets = np.array(tweets)
+    labels = np.array(labels)
+
+    train_indices, val_indices = split_train_test(tweets, seed=1)
+
+    # Build vocabulary
+    vocab = build_vocab(tweets)
+
+    # Transform the tweets into TF-IDF features
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(tweets)
+
+    X_train = X[train_indices]
+    X_val = X[val_indices]
+
+    Y_train = labels[train_indices]
+    Y_val = labels[val_indices]
 
     model = MultinomialNB()
     model.fit(X_train, Y_train)
 
-    model_features = model.coef_[0]
-    mapping = vectorizer.get_feature_names_out()
+    # Feature log probabilities
+    model_features = model.feature_log_prob_
 
-    get_top_pos_neg(model_features, mapping, 10)
+    # We need to subtract the log probabilities of the negative class from the positive class
+    feature_diff = model_features[1] - model_features[0]
 
+    # Get the top positive and negative words
+    get_top_pos_neg(feature_diff, vectorizer.get_feature_names_out(), k=10)
+
+    # Predict on validation set
     y_pred = model.predict(X_val)
 
+    # Print metrics
     get_basic_metrics(y_pred, Y_val)
+
+
+if __name__ == '__main__':
+    main()

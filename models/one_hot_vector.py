290272d2f82880448f03aecf2fa32dc0c88d0081
Li Ting Luong <lluong@student.ethz.ch>
2024-07-18 20:15:11 +0200

    cleaned up one_hot_vector code

--- models/one_hot_vector.py
+++ models/one_hot_vector.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python3
 import pickle
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from tqdm import tqdm
-from collections import Counter
-
-
-# build vocabulary
-def build_vocab(tweets):
-    vocabulary = {}
-    tokens = []
-    for tweet in tweets:
-        for word in tweet.split():
-            tokens.append(word)
-
-    # We only keep the 5000 most frequent words, both to reduce the computational cost and reduce overfitting
-    counter = Counter(tokens)
-    most_common_words = counter.most_common(5000)
-    most_common_words_len = len(most_common_words)
-
-    for index in range(most_common_words_len):
-        word = most_common_words[index][0]
-        vocabulary[word] = index
-    return vocabulary
+from utils import load_tweets, split_train_test, get_top_pos_neg, get_basic_metrics, build_vocab
 
 
 # train machine learning model to predict the label
@@ -41,14 +25,8 @@ def main():
     tweets = []
     labels = []
 
-    def load_tweets(filename, label):
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                tweets.append(line.rstrip())
-                labels.append(label)
-
-    load_tweets('data/train_neg.txt', 0)
-    load_tweets('data/train_pos.txt', 1)
+    load_tweets('../data/train_neg.txt', 0, tweets, labels)
+    load_tweets('../data/train_pos.txt', 1, tweets, labels)
     # load_tweets('data/train_neg_full.txt', 0)
     # load_tweets('data/train_pos_full.txt', 1)
 
@@ -56,12 +34,7 @@ def main():
     tweets = np.array(tweets)
     labels = np.array(labels)
 
-    np.random.seed(1)  # Reproducibility!
-
-    shuffled_indices = np.random.permutation(len(tweets))
-    split_idx = int(0.9 * len(tweets))
-    train_indices = shuffled_indices[:split_idx]
-    val_indices = shuffled_indices[split_idx:]
+    train_indices, val_indices = split_train_test(tweets, seed=1)
 
     # build vocabulary
     vocab = build_vocab(tweets)
@@ -103,12 +76,11 @@ def main():
         print(mapping[i], model_features[i])
     print()
 
+    # Predict on validation set
     y_pred = model.predict(X_val)
 
-    print("ACCURACY: " + str(accuracy_score(Y_val, y_pred)))
-    print("RECALL: " + str(recall_score(Y_val, y_pred)))
-    print("F1: " + str(f1_score(Y_val, y_pred)))
-    print("PRECISION: " + str(precision_score(Y_val, y_pred)))
+    # Print metrics
+    get_basic_metrics(y_pred, Y_val)
 
 
 if __name__ == '__main__':

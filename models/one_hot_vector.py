c994582b5d55cc7b7fff49610a0ddcd04039b371
Li Ting Luong <lluong@student.ethz.ch>
2024-07-25 21:49:47 +0200

    updated one_hot_vector.py code according to utils.py

--- models/one_hot_vector.py
+++ models/one_hot_vector.py
@@ -6,20 +6,10 @@ import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from tqdm import tqdm
-from utils import load_tweets, get_basic_metrics, build_vocab
+from utils import load_tweets, get_basic_metrics, build_vocab, split_train_test
 from preprocessing import preprocess
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-# split into training and validation set
-def split_train_test(tweets, seed):
-    np.random.seed(seed)  # reproducibility
-    shuffled_indices = np.random.permutation(len(tweets))
-    split_idx = int(0.9 * len(tweets))
-    train_indices = shuffled_indices[:split_idx]
-    val_indices = shuffled_indices[split_idx:]
-    return train_indices, val_indices
 
 
 # converts tweet to a one-hot vector based on vocabulary
@@ -32,45 +22,8 @@ def convert_to_one_hot_vec(tweet, vocabulary):
     return vector
 
 
-def main():
-    tweets = []
-    labels = []
-
-    # load tweets
-    load_tweets('../data/train_neg.txt', 0, tweets, labels)
-    load_tweets('../data/train_pos.txt', 1, tweets, labels)
-    # load_tweets('data/train_neg_full.txt', 0)
-    # load_tweets('data/train_pos_full.txt', 1)
-    preprocess(tweets, labels)
-
-    # convert to NumPy array to facilitate indexing
-    tweets = np.array(tweets)
-    labels = np.array(labels)
-
-    train_indices, val_indices = split_train_test(tweets, seed=1)
-
-    # build vocabulary
-    vocab = build_vocab(tweets)
-
-    # we only keep the 5000 most frequent words, both to reduce the computational cost and reduce overfitting
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    vectorizer.fit_transform(tweets)
-
-    # convert tweets to one-hot vectors
-    X_train = []
-    X_val = []
-    for tweet in tqdm(tweets[train_indices], desc="Converting training set"):
-        X_train.append(convert_to_one_hot_vec(tweet, vocab))
-    for tweet in tqdm(tweets[val_indices], desc="Converting validation set"):
-        X_val.append(convert_to_one_hot_vec(tweet, vocab))
-
-    Y_train = labels[train_indices]
-    Y_val = labels[val_indices]
-
-    # train logistic regression model
-    model = LogisticRegression(C=1e5, max_iter=100)
-    model.fit(X_train, Y_train)
-
+# get top 10 negative and positive features
+def get_features_ohv(model, vectorizer):
     model_features = model.coef_[0]
     # increasing order
     sorted_features = np.argsort(model_features)
@@ -91,8 +44,48 @@ def main():
         print(mapping[i], model_features[i])
     print()
 
+
+def main():
+    tweets = []
+    labels = []
+
+    # load tweets
+    load_tweets('../data/twitter-datasets/train_neg.txt', 0, tweets, labels)
+    load_tweets('../data/twitter-datasets/train_pos.txt', 1, tweets, labels)
+    # load_tweets('../data/twitter-datasets/train_neg_full.txt', 0, tweets, labels)
+    # load_tweets('../data/twitter-datasets/train_pos_full.txt', 1, tweets, labels)
+    preprocess(tweets, labels)
+
+    # convert to NumPy array to facilitate indexing
+    tweets = np.array(tweets)
+    labels = np.array(labels)
+
+    X_train, X_val, Y_train, Y_val = split_train_test(np.array(tweets), np.array(labels), 1)
+
+    # build vocabulary
+    vocab = build_vocab(tweets)
+
+    # we only keep the 5000 most frequent words, both to reduce the computational cost and reduce overfitting
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    vectorizer.fit_transform(tweets)
+
+    # convert tweets to one-hot vectors
+    X_train_ohv = []
+    X_val_ohv = []
+    for tweet in tqdm(X_train, desc="Converting training set"):
+        X_train_ohv.append(convert_to_one_hot_vec(tweet, vocab))
+    for tweet in tqdm(X_val, desc="Converting validation set"):
+        X_val_ohv.append(convert_to_one_hot_vec(tweet, vocab))
+
+    # train logistic regression model
+    model = LogisticRegression(C=1e5, max_iter=100)
+    model.fit(X_train_ohv, Y_train)
+
+    # print features
+    get_features_ohv(model, vectorizer)
+
     # predict on validation set
-    y_pred = model.predict(X_val)
+    y_pred = model.predict(X_val_ohv)
 
     # print metrics
     get_basic_metrics(y_pred, Y_val)
